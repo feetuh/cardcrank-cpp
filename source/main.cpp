19f79d55593aa78aa1f38ab2af39edaf2020c242
feetuh <feetuh.md@gmail.com>
2026-06-07 13:47:27 +0100

    feat: main

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <string>
-
-#include "lib.hpp"
+#include "crank/lib.hpp"
 
 auto main() -> int
 {
-  auto const lib = library {};
-  auto const message = "Hello from " + lib.name + "!";
-  std::cout << message << '\n';
+  // Create a 2-player game
+  crank::GameConfig config {2, std::uint32_t {42}};  // 2 players, seeded for reproducibility
+  crank::GameLibrary game {config};
+
+  // Start the game
+  game.start();
+
+  std::cout << "Card Crank game started with " << config.player_count << " players!\n";
+
+  // Get initial game view
+  auto view = game.get_view();
+  std::cout << "Current player: " << view.player_names[view.current_player_index] << "\n";
+  std::cout << "Stack size: " << view.stack_size << "\n";
+
   return 0;
 }
